71bc188fa625e44dde0d6fa40406986712388100
Charik Zakaria <charikzakaria@MacBook-Pro-de-Charik.local>
2018-12-24 18:19:48 +0100

    Fixture

--- BuySellCars/platforms/android/platform_www/cordova_plugins.js
+++ BuySellCars/platforms/android/platform_www/cordova_plugins.js
@@ -369,6 +369,14 @@ module.exports = [
       "navigator.notification",
       "navigator"
     ]
+  },
+  {
+    "id": "cordova-call.CordovaCall",
+    "file": "plugins/cordova-call/www/CordovaCall.js",
+    "pluginId": "cordova-call",
+    "clobbers": [
+      "cordova.plugins.CordovaCall"
+    ]
   }
 ];
 module.exports.metadata = 
@@ -384,7 +392,8 @@ module.exports.metadata =
   "cordova-plugin-globalization": "1.0.9",
   "cordova-plugin-inappbrowser": "1.7.2",
   "cordova-plugin-network-information": "1.3.4",
-  "cordova-plugin-vibration": "2.1.6"
+  "cordova-plugin-vibration": "2.1.6",
+  "cordova-call": "1.1.6"
 };
 // BOTTOM OF METADATA
 });
